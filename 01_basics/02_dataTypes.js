001a45373ab7e8086c3d72d2c7379bc4987238b6
Trip-Harshit2505 <113983830+Trip-Harshit2505@users.noreply.github.com>
2024-01-31 07:00:39 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,23 @@
+"use strict"; // use all JS code as newer version
+
+//alert( 3 + 3 ) // we are using nodejs not the browser
+
+let name = "Harshit"
+let age = 21
+let isLoggedIn = false
+let state;
+
+// number => 2 to the power of 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined
+// symbol => unique
+
+//object
+
+console.log(typeof "Harshit");
+console.log(typeof age);
+console.log(typeof null); // type object
+console.log(typeof undefined); // type undefined
