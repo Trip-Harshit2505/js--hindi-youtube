46f98eb38aa3dbedd661ba3960d0141304946880
Trip-Harshit2505 <113983830+Trip-Harshit2505@users.noreply.github.com>
2024-01-31 07:22:08 +0000

    Conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,25 @@
+// let score = "33abc"
+let score = null
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber = Number(score)
+
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+let isLogggedIn = 1
+let booleanIsLoggedIn = Boolean(isLogggedIn)
+
+// console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "harshit" => true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+
+console.log(stringNumber);
+console.log(typeof stringNumber);
