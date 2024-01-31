980df2408e2b3af4550360d41a1b0b538d655718
Trip-Harshit2505 <113983830+Trip-Harshit2505@users.noreply.github.com>
2024-01-31 08:00:42 +0000

    Operation

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -21,5 +21,31 @@ let booleanIsLoggedIn = Boolean(isLogggedIn)
 let someNumber = 33
 let stringNumber = String(someNumber)
 
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// ***********Operation***********
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+// console.log(1 + "2");
+// console.log("1" + 2);
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log(+true);
+// console.log(+"");
+
+let gameCounter = 100
+gameCounter++;
+
+console.log(gameCounter);
